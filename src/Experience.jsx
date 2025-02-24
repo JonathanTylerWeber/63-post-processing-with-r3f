16f89acb7e799044aed09873064d81900cae1785
Jonathan Tyler Weber <jonathantweber@gmail.com>
2025-02-24 15:58:26 +0800

    done

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,31 +1,99 @@
-import { OrbitControls } from '@react-three/drei'
-import { Perf } from 'r3f-perf'
+import { OrbitControls } from "@react-three/drei";
+import { Perf } from "r3f-perf";
+import {
+  Bloom,
+  DepthOfField,
+  EffectComposer,
+  Glitch,
+  Noise,
+  ToneMapping,
+  Vignette,
+} from "@react-three/postprocessing";
+import { BlendFunction, GlitchMode, ToneMappingMode } from "postprocessing";
+import Drunk from "./Drunk";
+import { useRef } from "react";
+import { useControls } from "leva";
 
-export default function Experience()
-{
-    return <>
+export default function Experience() {
+  const drunkRef = useRef();
 
-        <Perf position="top-left" />
+  const drunkProps = useControls("Drunk Effect", {
+    frequency: { value: 2, min: 1, max: 20 },
+    amplitude: { value: 0.1, min: 0, max: 1 },
+  });
 
-        <OrbitControls makeDefault />
+  return (
+    <>
+      <color args={["#fff"]} attach="background" />
 
-        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
-        <ambientLight intensity={ 1.5 } />
+      <EffectComposer>
+        <ToneMapping mode={ToneMappingMode.ACES_FILMIC} />
 
-        <mesh castShadow position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
+        {/* <Vignette
+          offset={0.3}
+          darkness={0.9}
+          blendFunction={BlendFunction.NORMAL}
+        /> */}
 
-        <mesh castShadow position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
+        {/* <Glitch
+          delay={[0.5, 1]}
+          duration={[0.1, 0.3]}
+          strength={[0.2, 0.4]}
+          mode={GlitchMode.CONSTANT_WILD}
+        /> */}
 
-        <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
-            <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
+        {/* <Noise premultiply blendFunction={BlendFunction.SOFT_LIGHT} /> */}
 
+        {/* <Bloom luminanceThreshold={1.1} mipmapBlur intensity={0.5} /> */}
+
+        {/* <DepthOfField
+          focusDistance={0.025}
+          focalLength={0.025}
+          bokehScale={6}
+        /> */}
+
+        <Drunk
+          ref={drunkRef}
+          {...drunkProps}
+          blendFunction={BlendFunction.DARKEN}
+        />
+      </EffectComposer>
+
+      <Perf position="top-left" />
+
+      <OrbitControls makeDefault />
+
+      <directionalLight castShadow position={[1, 2, 3]} intensity={4.5} />
+      <ambientLight intensity={1.5} />
+
+      <mesh castShadow position-x={-2}>
+        <sphereGeometry />
+        <meshStandardMaterial color="orange" />
+      </mesh>
+
+      <mesh castShadow position-x={2} scale={1.5}>
+        <boxGeometry />
+        <meshStandardMaterial
+          color="mediumpurple"
+          // emissive="mediumpurple"
+          // emissiveIntensity={2}
+        />
+      </mesh>
+
+      {/* <mesh castShadow position-x={2} scale={1.5}>
+        <boxGeometry />
+        <meshBasicMaterial color={[1.5, 1, 4]} />
+      </mesh> */}
+
+      <mesh
+        receiveShadow
+        position-y={-1}
+        rotation-x={-Math.PI * 0.5}
+        scale={10}
+      >
+        <planeGeometry />
+        <meshStandardMaterial color="greenyellow" />
+      </mesh>
     </>
+  );
 }
